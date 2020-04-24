4fb9397801584c176e664645b68a77a0a66aedeb
Emma Finkel <emma.finkel@gmail.com>
2020-04-24 11:01:05 -0400

    Complete Iteration 1

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Emojional
-//
-//  Created by Emma Finkel on 4/24/20.
-//  Copyright © 2020 Emma Finkel. All rights reserved.
-//
 
 import UIKit
 
@@ -15,6 +8,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        
+        // when a user clicks on a button, a randomly generated message should pop up in the alert box
+        // related to how the person is feeling
+        
+        let alertController = UIAlertController(title: "We all get EMOJIonal!", message: "You got this!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Thanks! I'm feeling better!", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
